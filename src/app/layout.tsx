6ce85bdd3571111d6f1f8b698fe9d55e442edda1
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2025-05-22 01:14:06 +0200

    metadata title update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Jobmatch",
+  title: "Oppi",
   description: "Matching job seekers and recruiters",
 };
 
