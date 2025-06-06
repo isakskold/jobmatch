5a08bcb13f4571e764957382e8d1ec5ad63e77d0
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2025-06-06 23:50:06 +0200

    Removed height from page layout component

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -8,7 +8,7 @@ export default function PageLayout({
   className = "",
 }: PageLayoutProps) {
   return (
-    <div className="min-h-screen">
+    <div className="">
       <main
         className={`max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 ${className}`}
       >
